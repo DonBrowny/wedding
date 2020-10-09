5c170b106c724430c30b3057685c55d88fc3959e
DonBrowny <kishore13ask@gmail.com>
2020-10-09 22:39:37 +0530

    added baseref to route

---
+++ precache-manifest.09c317652d7d1130af61b08f702363f1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e4e9d68c58383a24787e8ca54726d7c",
+    "url": "/wedding/index.html"
+  },
+  {
+    "revision": "bfcf0887099324267c66",
+    "url": "/wedding/static/css/2.42ec2a3e.chunk.css"
+  },
+  {
+    "revision": "5749db046ace76a787f3",
+    "url": "/wedding/static/css/main.ab211b11.chunk.css"
+  },
+  {
+    "revision": "bfcf0887099324267c66",
+    "url": "/wedding/static/js/2.07f3bedb.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/wedding/static/js/2.07f3bedb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5749db046ace76a787f3",
+    "url": "/wedding/static/js/main.07aecde1.chunk.js"
+  },
+  {
+    "revision": "31dfd0da05b801feb06a",
+    "url": "/wedding/static/js/runtime-main.fc848693.js"
+  }
+]);
